598f72adb78a0735ea8b3d14321b5c61ee1a157f
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-02-05 07:06:30 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,16 +2,15 @@
 
 rm -rf .repo/local_manifests
 
-repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs
-
-git clone https://github.com/SenseiDEVLinux/LocalManifest --depth 1 -b Evo14 .repo/local_manifests
+repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+git clone https://github.com/SenseiDEVLinux/LocalManifest --depth 1 -b los21 .repo/local_manifests
 
 /opt/crave/resync.sh
 
 echo -e "\e[33m \033[1m LET'S GO \e[0m \033[0m"
 
 . build/envsetup.sh
-lunch lineage_lavender-user
-m evolution
+lunch lineage_lavender-userdebug
+m bacon
 
 curl bashupload.com -T out/target/product/lavender/EvolutionX*.zip
