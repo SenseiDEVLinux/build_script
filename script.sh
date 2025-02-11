d532bf11dea882db6e2b825c98b5b850df619013
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-02-11 19:53:34 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,12 +2,12 @@
 
 rm -rf .repo/local_manifests
 
-repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
 
 git clone https://github.com/SenseiDEVLinux/LocalManifest --depth 1 -b yaap .repo/local_manifests
 
 /opt/crave/resync.sh
 
 . build/envsetup.sh
-lunch yaap_lavender-userdebug
-m yaap
+lunch lineage_lavender-ap4a-userdebug
+m evolution
