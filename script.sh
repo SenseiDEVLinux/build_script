8a072409878695b4db32b55e4615c4bc61d757bb
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-03-08 14:20:36 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 
 rm -rf .repo/local_manifests
 
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
-git clone https://github.com/SenseiDEVLinux/LocalManifest --depth 1 -b los21-garnet .repo/local_manifests
+repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 15 --git-lfs
+git clone https://github.com/SenseiDEVLinux/LocalManifest --depth 1 -b sacura .repo/local_manifests
 
 /opt/crave/resync.sh
 
 . build/envsetup.sh
-lunch lineage_garnet-ap2a-userdebug
-mka bacon
+lunch lineage_garnet-ap4a-userdebug
+make bacon
