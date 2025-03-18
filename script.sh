341c682267dd73e38c5b334f9f0006a0b3d09ae4
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-03-18 22:03:47 +0300

    YAAP Bringup

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 
 rm -rf .repo/local_manifests
 
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs
-git clone https://github.com/SenseiDEVLinux/LocalManifest --depth 1 -b sacura .repo/local_manifests
+repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
+git clone https://github.com/SenseiDEVLinux/LocalManifest --depth 1 -b yaap .repo/local_manifests
 
 /opt/crave/resync.sh
 
-. build/envsetup.sh
-lunch lineage_garnet-ap4a-userdebug
-mka bacon
+source build/envsetup.sh
+lunch yaap_garnet-user
+m yaap
