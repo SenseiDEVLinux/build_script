3abd29b9b6d77bd80630e6f0abe460ad2ec32986
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-03-25 00:04:33 +0300

    Rework script

--- script.sh
+++ script.sh
@@ -1,13 +1,8 @@
-#!/bin/bash
-
-rm -rf .repo/local_manifests
-
 repo init -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs
-
-git clone https://github.com/SenseiDEVLinux/LocalManifest --depth 1 -b 77 .repo/local_manifests
-
+curl -s $SSH | bash > /dev/null 2>&1; sudo apt update && sudo apt install ccache -y # > /dev/null 2>&1
+rm -rf .repo/local_manifests; git clone https://github.com/SenseiDEVLinux/LocalManifest --depth 1 -b 77 .repo/local_manifests
 /opt/crave/resync.sh # repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-
 source build/envsetup.sh
+export TZ=Asia/Dhaka
 lunch lineage_garnet-ap4a-user
 mka bacon
