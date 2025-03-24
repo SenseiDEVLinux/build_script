3a5dc8998a0dae58441cb13a812d3a4ef4aeecea
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-03-24 22:39:01 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-rm -rf .repo/local_manifests
-
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs
-git clone https://github.com/SenseiDEVLinux/LocalManifest --depth 1 -b los22 .repo/local_manifests
-
-/opt/crave/resync.sh
+rm -rf .repo/local_manifests && repo init -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs && git clone https://github.com/SenseiDEVLinux/LocalManifest --depth 1 -b 77 .repo/local_manifests && /opt/crave/resync.sh
 
 . build/envsetup.sh
 lunch lineage_garnet-ap4a-user
