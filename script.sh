0d1096b5db8e2dbb15bffbac857afb415543c2a1
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-03-24 22:46:58 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,6 +8,12 @@ git clone https://github.com/SenseiDEVLinux/LocalManifest --depth 1 -b 77 .repo/
 
 /opt/crave/resync.sh
 
+rm -rf hardware
+
+rm -rf device/qcom/sepolicy_vndr
+
+git clone https://github.com/LineageOS/android_device_qcom_sepolicy_vndr device/qcom/sepolicy_vndr/sm8450
+
 . build/envsetup.sh
 lunch lineage_garnet-ap4a-user
 mka bacon
