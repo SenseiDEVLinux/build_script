14211ea6687c5310c0a66aada8ed18b6cf8428a5
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-04-01 09:59:39 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,5 +8,5 @@ git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b crdroid
 /opt/crave/resync.sh
 
 . build/envsetup.sh
-lunch lineage_garnet-ap4a-user
+lunch lineage_garnet-ap4a-userdebug
 mka bacon
