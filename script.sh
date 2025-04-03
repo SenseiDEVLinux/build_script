bb10e9aedb4150e93f22d90f91236af5e1cb2c84
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-04-03 23:51:48 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,5 +8,5 @@ git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b yaap-15
 /opt/crave/resync.sh
 
 . build/envsetup.sh
-lunch yaap_garnet-ap4a-eng
+lunch yaap_garnet-ap4a-userdebug
 m yaap
