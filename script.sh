dfac6548a4171d0a7c62ddbf962bd8df0d5bf489
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-04-06 17:13:38 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 
 rm -rf .repo/local_manifests
 
-repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
-git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b yaap-15 .repo/local_manifests
+repo init -u https://github.com/The-Pixel-Project/manifest -b 15 --git-lfs
+git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b pixel .repo/local_manifests
 
 /opt/crave/resync.sh
 
 . build/envsetup.sh
-lunch yaap_garnet-bp1a-userdebug
-m yaap
+lunch aosp_garnet-bp1a-userdebug
+make bacon
