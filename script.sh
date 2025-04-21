b58c2d9e1141af1c7b44550c491d2ada503f0dd8
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-04-21 20:31:08 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,11 +2,10 @@
 
 rm -rf .repo/local_manifests
 
-repo init -u https://github.com/Pixel-Project-Staging/manifest -b 15-pfw --git-lfs
-git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b pixel .repo/local_manifests
+repo init -u https://github.com/BlissRoms/stable_releases.git -b refs/tags/v18.5-stable-voyager --git-lfs
+git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b bliss .repo/local_manifests
 
 /opt/crave/resync.sh
 
 . build/envsetup.sh
-lunch aosp_garnet-bp1a-userdebug
-make bacon
+blissify -v garnet
