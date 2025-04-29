9d5dd3e8714cc6ae9c06de7155ee4047111694f9
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-04-29 22:38:04 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 
 rm -rf .repo/local_manifests
 
-repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
 git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b evo .repo/local_manifests
 
 /opt/crave/resync.sh
 
 . build/envsetup.sh
 lunch lineage_garnet-bp1a-userdebug
-m evolution
+mka bacon
