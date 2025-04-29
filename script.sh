ec280c438791912b176d6f1494432ec3d155b166
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-04-29 16:40:23 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 
 rm -rf .repo/local_manifests
 
-repo init -u https://github.com/The-Pixel-Project/manifest -b 15 --git-lfs
-git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b pixel .repo/local_manifests
+repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
+git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b evo .repo/local_manifests
 
 /opt/crave/resync.sh
 
 . build/envsetup.sh
-lunch aosp_garnet-bp1a-userdebug
-make bacon
+lunch lineage_garnet-bp1a-userdebug
+m evolution
