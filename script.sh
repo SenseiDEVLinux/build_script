cab5eb45970c12c603a9e9901996f54c027566fa
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-05-04 23:10:51 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 
 rm -rf .repo/local_manifests
 
-repo init -u https://github.com/The-Pixel-Project/manifest -b 15 --git-lfs
+repo init -u https://github.com/RisingOS-Revived/android -b fifteen --git-lfs
 git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b pixel .repo/local_manifests
 
 /opt/crave/resync.sh
 
 . build/envsetup.sh
-lunch aosp_garnet-bp1a-userdebug
-make bacon
+riseup garnet userdebug
+rise b
