0adeac06b5dbc245b331033b6c42ae39e86ab26d
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-05-06 07:05:54 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,10 +3,10 @@
 rm -rf .repo/local_manifests
 
 repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
-git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b lineage-22.1 .repo/local_manifests
+git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b matrixx .repo/local_manifests
 
 /opt/crave/resync.sh
 
 . build/envsetup.sh
-lunch lineage_garnet-ap4a-userdebug
+lunch lineage_garnet-bp1a-userdebug
 brunch garnet
