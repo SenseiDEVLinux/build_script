4a6fef551457a4d4ddeb82dc286630c7396d940c
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-05-07 23:36:00 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 rm -rf .repo/local_manifests
 
-repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
+repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
 git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b matrixx .repo/local_manifests
 
 /opt/crave/resync.sh
