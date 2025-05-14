4e451cf840bf405ef7dccea2c917b891198d4d6f
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-05-14 11:06:55 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,10 +4,10 @@ rm -rf .repo/local_manifests
 
 repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
 
-git clone https://github.com/SenseiDEVLinux/LocalManifest --depth 1 -b yaap .repo/local_manifests
+git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b ris .repo/local_manifests
 
 /opt/crave/resync.sh
 
 . build/envsetup.sh
-lunch lineage_lavender-ap4a-userdebug
+lunch lineage_garnet-bp1a-userdebug
 m evolution
