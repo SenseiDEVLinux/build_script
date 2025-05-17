b943b84c98ccda00b859f57e26f9e6dc4a4a523e
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-05-17 09:42:53 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 rm -rf .repo/local_manifests
 
 repo init -u https://github.com/The-Pixel-Project/manifest -b 15 --git-lfs
-git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b p .repo/local_manifests
+git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b yaap .repo/local_manifests
 
 /opt/crave/resync.sh
 
