bd13a2bab3436de04e689707d2a664c832e49682
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-07-02 06:52:14 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 rm -rf .repo/local_manifests
 
 repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs
-git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b ris15 .repo/local_manifests
+git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b crdroid-15 .repo/local_manifests
 
 /opt/crave/resync.sh
 
