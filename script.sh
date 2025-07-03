f902064a0265297e7adef6fe0c4a9e5734999fd3
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-07-03 21:21:31 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,10 +3,10 @@
 rm -rf .repo/local_manifests
 
 repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
-git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b crdroid-15 .repo/local_manifests
+git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b cr15-saphire .repo/local_manifests
 
 /opt/crave/resync.sh
 
 . build/envsetup.sh
-lunch lineage_garnet-bp1a-userdebug
+lunch lineage_saphhire-bp1a-userdebug
 mka bacon
