cc12200a6750d31bf4d29780dd317177f41fb94c
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-07-17 19:47:56 +0300

    los23

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@
 
 rm -rf .repo/local_manifests
 
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
-git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b crdroid-15 .repo/local_manifests
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
+git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b a16 .repo/local_manifests
 
 /opt/crave/resync.sh
 
