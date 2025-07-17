ae25a4de4c260e787f8f4a30a8bc7418b20d8a6f
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-07-17 07:10:57 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 rm -rf .repo/local_manifests
 
 repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
-git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b evo-oss .repo/local_manifests
+git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b crdroid-15 .repo/local_manifests
 
 /opt/crave/resync.sh
 
