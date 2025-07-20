ae7c5f0dc278851f5a205cb1f01395b208054473
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-07-20 08:47:06 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 rm -rf .repo/local_manifests
 
-repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
+repo init -u https://github.com/SenseiDEVLinux/android1.git -b 15.0 --git-lfs
 git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b crdroid-15 .repo/local_manifests
 
 /opt/crave/resync.sh
