686bc5f8d232ad251672ccfaaf9e2a064c8c4589
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-07-26 16:17:52 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,8 @@
 
 rm -rf .repo/local_manifests
 
+rm -rf prebuilts/clang/host/linux-x86
+
 repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
 git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b crdroid-15 .repo/local_manifests
 
