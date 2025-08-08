439b6ca737a53b9e8a4520cab7102a77e14df58e
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-08-08 23:37:22 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,11 +4,15 @@ rm -rf .repo/local_manifests
 
 rm -rf prebuilts/clang/host/linux-x86
 
-repo init -u https://github.com/SenseiDEVLinux/android1.git -b 15.0 --git-lfs
-git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b crdroid-15 .repo/local_manifests
+cd vendor/voltage-priv/keys
+
+./keys.sh
+
+repo init -u https://github.com/VoltageOS/manifest.git -b 15-qpr2 --git-lfs
+git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b volt15 .repo/local_manifests
 
 /opt/crave/resync.sh
 
 . build/envsetup.sh
-lunch lineage_garnet-bp1a-userdebug
+lunch voltage_garnet-bp1a-userdebug
 mka bacon
