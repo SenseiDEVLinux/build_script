399d8dd53ba4eab5526fbc6a88b4f05be99c7033
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-08-14 22:21:19 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,11 +4,11 @@ rm -rf .repo/local_manifests
 
 rm -rf prebuilts/clang/host/linux-x86
 
-repo init -u https://github.com/SenseiDEVLinux/android1.git -b 15.0 --git-lfs
-git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b crdroid-15 .repo/local_manifests
+repo init -u https://github.com/SenseiDEVLinux/platform_manifest_aicp -b v15.0 --git-lfs
+git clone https://github.com/SenseiDEVLinux/local_manifests --depth 1 -b aicp .repo/local_manifests
 
 /opt/crave/resync.sh
 
 . build/envsetup.sh
-lunch lineage_garnet-bp1a-userdebug
+lunch aicp_garnet-bp1a-userdebug
 mka bacon
